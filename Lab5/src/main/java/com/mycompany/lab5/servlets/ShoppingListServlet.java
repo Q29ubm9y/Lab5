b49d7e5fccba950bfeb009cedbe02a4064495a4c
Q29ubm9y <97636630+Q29ubm9y@users.noreply.github.com>
2022-02-18 13:08:47 -0700

    Add files via upload

---
+++ Lab5/src/main/java/com/mycompany/lab5/servlets/ShoppingListServlet.java
@@ -0,0 +1,80 @@
+package com.mycompany.lab5.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Connor
+ */
+@WebServlet(name = "ShoppingListServlet", urlPatterns = {"/ShoppingList"})
+public class ShoppingListServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        String query = request.getQueryString();
+        
+        if (query != null && query.contains("logout")) {
+            session.invalidate();
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        }
+        
+        if (name == null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        HttpSession session = request.getSession();
+        
+        if (action != null && action.equals("add")) {
+            String item = request.getParameter("item");
+            ArrayList<String> items = (ArrayList<String>) session.getAttribute("items");
+            items.add(item);
+            session.setAttribute("items", items);
+        } else if (action != null && action.equals("register")) {
+            String name = request.getParameter("name");
+            ArrayList<String> items = new ArrayList<>();
+            session.setAttribute("name", name);
+            session.setAttribute("items", items);
+        } else if (action != null && action.equals("delete")) {
+            String item = request.getParameter("item");
+            ArrayList<String> items = (ArrayList<String>) session.getAttribute("items");
+            items.remove(item);
+            session.setAttribute("items", items);
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+    }
+
+}
